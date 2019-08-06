4f2e2432b6f49586d152f586f368ae14a4dc96e5
Sabin <xresta96@gmail.com>
2019-08-06 18:42:32 +0545

    Initialised Project

--- app/src/main/java/com/example/mixxtunes/MainActivity.java
+++ app/src/main/java/com/example/mixxtunes/MainActivity.java
@@ -1,18 +1,65 @@
 package com.example.mixxtunes;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ListView myListViewforSongs;
+    Button play, pause, stop;
+    MediaPlayer mediaPlayer;
+    int pauseCurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myListViewforSongs = (ListView) findViewById(R.id.mySongsListView);
+        play = (Button) findViewById(R.id.btn_play);
+        pause = (Button) findViewById(R.id.btn_pause);
+        stop = (Button) findViewById(R.id.btn_stop);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.btn_play:
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.album_art);
+                    mediaPlayer.start();
+                } else if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+                }
+
+                break;
+
+            case R.id.btn_pause:
+                if (mediaPlayer != null) {
+                    mediaPlayer.pause();
+                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                }
+                break;
+
+            case R.id.btn_stop:
+                if (mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer = null;
+                }
+                break;
+
+        }
+
     }
 }
